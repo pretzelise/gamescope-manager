e2272c28d5a81ca87e9971be41abcfa5e8fb30a2
pretzelise <mail@elise.gg>
2025-11-17 19:53:12 +1000

    Updated gamescope-post-cmd.sh to check for $SteamAppId env variable when locating the gamescope process

--- .config/gamescope-manager/gamescope-post-cmd.sh
+++ .config/gamescope-manager/gamescope-post-cmd.sh
@@ -3,12 +3,12 @@
 gamescope_launch_command=$1   # the command string used to identify the upcoming gamescope process
 post_run_cmd=("${@:2}")       # the command to run after gamescope has exited
 start_timestamp=$EPOCHSECONDS # store the current time so we can exit after 30 seconds of no activity
-gamescope_pid=
+gamescope_pid=""
 
 until [ $((EPOCHSECONDS - start_timestamp)) -gt 30 ]; do
   sleep 1 &
   # get the pid of the new gamescope process if it exists
-  gamescope_pid=$(pgrep gamescope --list-full | grep -v grep | grep "$gamescope_launch_command" | awk '{print $1}')
+  gamescope_pid=$(pgrep gamescope --list-full | grep -v grep | grep "$gamescope_launch_command" | grep "AppId=$SteamAppId" | awk '{print $1}') # uses $SteamAppId env var to confirm its found the correct gamescope process
   wait
 
   # if gamescope's pid has been found, exit the loop and continue the script
